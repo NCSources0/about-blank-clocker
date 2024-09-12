531680f7726216556627b4164a50ba558b65a823
NCSources0 <ncsources0@gmail.com>
2024-09-11 20:10:08 -0400

    Fix it to purrfection
    *purr*

--- cloak.js
+++ cloak.js
@@ -1,17 +1,36 @@
 function cloak() {
   try {
-    const domain = window.location.origin;
-    const aboutBlank = window.open("about:blank", "_blank");
+    const aboutBlank = open();
     if (!aboutBlank) {
-      throw new Error("Failed to open new window");
+      throw new Error("Failed to open about:blank");
     }
+    
     aboutBlank.document.open();
     aboutBlank.document.write(
-      ``
+      `<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8"/>
+    <meta name="viewport"content="width=device-width,initial-scale=1.0"/>
+  </head>
+  <body>
+    <iframe src="${location.href}"
+      style="
+        position:fixed;
+        width:100vw;
+        height:100vh;
+        left:0;
+        top:0;
+        border:0;
+      "></iframe>
+  </body>
+</html>`
     );
     aboutBlank.document.close();
+    
+    location.replace("http://google.co");
   } catch (error) {
-    console.error("Error loading about:blank.html", error);
+    console.error(error);
     alert("Error: " + error.message);
   }
 }
